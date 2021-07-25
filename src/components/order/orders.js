875f8fd8b5a81bda93193f5cb422f4fa49a1bfb6
saeid niyabaty <saeidniyabaty@gmail.com>
2021-07-25 16:55:46 +0430

    fixing the text lists in order

--- src/components/order/orders.js
+++ src/components/order/orders.js
@@ -5,8 +5,19 @@ import { Header } from './../base/ordersHeader'
 
 export const orders = () => {
     return (
+        <>
         <div className="div--container__orders">
-        <Header />
+        <Header/>
+        <section className="section--container__textlist">
+            <ul className="ul--container__order">
+                <li className="li--order">پیش غذا</li>
+                <li className="li--order">پیتزا</li>
+                <li className="li--order">نوشیدنی</li>
+                <li className="li--order">سالاد</li>
+                <li className="li--order">سس</li>
+            </ul>
+        </section>
         </div>
+        </>
     )
 }
