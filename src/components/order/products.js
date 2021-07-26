b3f492475fceca66166e5937dd92429df37ae7c5
saeid niyabaty <saeidniyabaty@gmail.com>
2021-07-26 19:12:32 +0430

    fixing control botton in order

--- src/components/order/products.js
+++ src/components/order/products.js
@@ -3,19 +3,20 @@ import { Carousel } from 'react-bootstrap';
 import { ControlButton } from './controlButton';
 import { Detail } from './detail';
 import { ToggleButton } from './toggleButton';
+import pizza from '../../assets/images/order/picc_pizza.png'
 
 export const Products = () => {
     return (
         <div className="div--container__product">
-        <Carousel nextLabel={false} prevLabel={false} interval={null} >
-  <Carousel.Item>
+        <Carousel nextLabel={false} prevLabel={false} interval={null} className="carousal--product" >
+  <Carousel.Item className="carousal--item__prouduct">
     <img
       className="d-block w-100"
-      src="https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGl6emF8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      src={pizza}
       alt="First slide"
     />
   </Carousel.Item>
-  <Carousel.Item>
+  {/* <Carousel.Item>
     <img
       className="d-block w-100"
       src="https://images.unsplash.com/photo-1571407970349-bc81e7e96d47?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBpenphfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
@@ -30,7 +31,7 @@ export const Products = () => {
       alt="Third slide"
     />
 
-  </Carousel.Item>
+  </Carousel.Item> */}
 </Carousel>
     <ToggleButton></ToggleButton>
     <Detail></Detail>
