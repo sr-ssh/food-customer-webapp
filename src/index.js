93f1609b1b4bdc44b377b6c69e15e67e56993166
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-07-28 08:39:24 +0430

    old address page done

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import './assets/styles/mainStyle.css'
 import './assets/styles/orders.css'
 import './assets/styles/factorStyle.css'
 import './assets/styles/orderdetailsStyle.css'
+import './assets/styles/addressStyles.css'
 
 ReactDOM.render(
   <Provider store={store}>
