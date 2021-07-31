ea7f15817196f0272e70f3bda55e83c133137c6e
saeid niyabaty <saeidniyabaty@gmail.com>
2021-07-31 20:01:42 +0430

    fixing prodct

--- src/components/order/products.js
+++ src/components/order/products.js
@@ -9,36 +9,46 @@ import { useDispatch, useSelector } from 'react-redux';
 import { orderAction } from '../../actions/orderAction';
 
 
+const data = [
+  {title:'نان سیر',img:pizza,description:"سیر . خمیر تازه . اویشن"},
+  {title:'پپرونی',img:"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg",description:"گوشت گوساله . پنیر . قارچ . فلفل دلمه ای . پیازجه"},
+  {title:'رست بیف',img:"https://iranmakimah.com/wp-content/uploads/2021/01/phut_0.jpg",description:"کالباس پپرونی . پنیر . قارچ . فلفل دلمه ای"}
+]
 
 export const Products = () => {
   const dispatch = useDispatch()
   const getProduct = useSelector(state => state.getProduct)
   const [index, setIndex] = useState(0);
+  const [title , setTitle] = useState('')
+  
   
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
-    // console.log(e.target.className==='carousel-control-prev-icon')
   };
-
+  
   
   useEffect(()=>dispatch(orderAction.getProduct()),[dispatch])
 
   
     return (
         <div className="div--container__product">
-        <Carousel  activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
+        <Carousel activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
+          {data.map(item=>
+
             <Carousel.Item className="carousal--item__prouduct">
+              
     <img
       className="d-block w-100"
-      src={getProduct.items?.data?.img}
+      src={item.img}
       alt="First slide"
       />
   </Carousel.Item>
 
+      )}
   
 </Carousel>
     <ToggleButton></ToggleButton>
-    <Detail></Detail>
+    <Detail detail={data[index]}></Detail>
     <ControlButton></ControlButton>
     <Dialog></Dialog>
         </div>
