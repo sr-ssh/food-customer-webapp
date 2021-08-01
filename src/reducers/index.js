0a4c9efebf56874576bb6e1c6b3ef420ba274f00
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-01 09:52:39 +0430

    fix reducer index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { authentication, register, verificationCode } from './authenticationReducer';
 import { alert } from './alertReducer';
 import { getAddresses, newAddress } from './addressReducer';
+import { getProduct } from "./orderReducer";
 export default combineReducers({
     authentication,
     register,
