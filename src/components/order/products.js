2690484223124b23777079dde5423f188096627a
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-02 16:25:49 +0430

    add product size in toggleButton

--- src/components/order/products.js
+++ src/components/order/products.js
@@ -1,56 +1,67 @@
 import React, { useEffect, useState } from 'react';
-import { Carousel } from 'react-bootstrap';
-import { ControlButton } from './controlButton';
-import { Detail } from './detail';
-import { ToggleButton } from './toggleButton';
-import pizza from '../../assets/images/order/picc_pizza.png'
-import { Dialog } from './dialog';
+import { Carousel, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { orderAction } from '../../actions/orderAction';
 
+//  Components
+import { ControlButton } from './controlButton';
+import { Detail } from './detail';
+import { ToggleButton } from './toggleButton';
+import { Dialog } from './dialog';
+
+// Assets
+import pizza from '../../assets/images/order/picc_pizza.png'
+
+
 
 const data = [
-  {title:'نان سیر',img:pizza,description:"سیر . خمیر تازه . اویشن"},
-  {title:'پپرونی',img:"https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg",description:"گوشت گوساله . پنیر . قارچ . فلفل دلمه ای . پیازجه"},
-  {title:'رست بیف',img:"https://iranmakimah.com/wp-content/uploads/2021/01/phut_0.jpg",description:"کالباس پپرونی . پنیر . قارچ . فلفل دلمه ای"}
+  { title: 'نان سیر', img: pizza, description: "سیر . خمیر تازه . اویشن" },
+  { title: 'پپرونی', img: "https://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg", description: "گوشت گوساله . پنیر . قارچ . فلفل دلمه ای . پیازجه" },
+  { title: 'رست بیف', img: "https://iranmakimah.com/wp-content/uploads/2021/01/phut_0.jpg", description: "کالباس پپرونی . پنیر . قارچ . فلفل دلمه ای" }
 ]
 
 export const Products = () => {
   const dispatch = useDispatch()
   const getProduct = useSelector(state => state.getProduct)
   const [index, setIndex] = useState(0);
-  const [title , setTitle] = useState('')
-  
-  
+  const [productSize, setProductSize] = useState('');
+
+  const [title, setTitle] = useState('')
+
+  console.log(productSize);
+
+
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
-  
-  
-  useEffect(()=>dispatch(orderAction.getProduct()),[dispatch])
 
-  
-    return (
-        <div className="div--container__product">
-        <Carousel activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
-          {data.map(item=>
 
-            <Carousel.Item className="carousal--item__prouduct">
-              
-    <img
-      className="d-block w-100"
-      src={item.img}
-      alt="First slide"
-      />
-  </Carousel.Item>
+  useEffect(() => dispatch(orderAction.getProduct()), [dispatch])
 
-      )}
-  
-</Carousel>
-    <ToggleButton></ToggleButton>
-    <Detail detail={data[index]}></Detail>
-    <ControlButton></ControlButton>
-    <Dialog></Dialog>
-        </div>
-    )
+
+  return (
+    <div className="div--container__product">
+      <Carousel activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
+        {data.map(item =>
+
+          <Carousel.Item className="carousal--item__prouduct">
+
+            <img
+              className="d-block w-100"
+              src={item.img}
+              alt="First slide"
+            />
+          </Carousel.Item>
+
+        )}
+
+      </Carousel>
+      <Row className="d-flex justify-content-center aling-align-items-center col-12 m-0 ">
+        <ToggleButton sizeProduct={setProductSize}></ToggleButton>
+      </Row>
+      <Detail detail={data[index]}></Detail>
+      <ControlButton></ControlButton>
+      <Dialog></Dialog>
+    </div>
+  )
 }
--- src/components/order/toggleButton.js
+++ src/components/order/toggleButton.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 
-export const ToggleButton = () => {
+export const ToggleButton = ({ sizeProduct }) => {
+
+    const [activeOption, setActiveOption] = useState(0)
+    const option = [{ 0: "1 نفره" }, { 1: "2 نفره" }]
+
+
+
+    let optionHandler = (e) => {
+        if (e == 0)
+            sizeProduct("meduim")
+        else if (e == 1)
+            sizeProduct("larg");
+        setActiveOption(e)
+    }
+
+
     return (
-        <div className="div--container__togglebutton">
-         <div className="div--container__togglebutton-child" >
-          <p className="div--container__togglebutton-child−text" >1 نفره</p>
-         </div>
-         <p className="div--container__togglebutton-text" >2 نفره</p>
-         </div>
+        <div className="div--container__togglebutton d-flex flex-row justify-content-around align-items-center px-2">
+            {
+                option.map((item, index) => <p className={`col-6 d-flex justify-content-around align-items-center div--container__togglebutton-text m-0 ${activeOption == index ? "active-opstion" : null}`} onClick={() => optionHandler(index)}>{item.[index]}</p>)
+            }
+        </div>
     )
 }
