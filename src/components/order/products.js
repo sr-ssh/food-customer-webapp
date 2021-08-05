2912b5449d7a94beb39227cdeb45f94de370ef31
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-05 13:02:32 +0430

    fix product size

--- src/components/order/products.js
+++ src/components/order/products.js
@@ -25,54 +25,56 @@ export const Products = () => {
   const dispatch = useDispatch()
   const data = useSelector(state => state.getProduct.products)
   const [index, setIndex] = useState(0);
-  const [number , setNumber] = useState(0)
-  const [orderList,setOrderList] = useState([...data])
-  
+  const [number, setNumber] = useState(0)
+  const [orderList, setOrderList] = useState([...data])
+  const [productSize, setProductSize] = useState('');
+
   // function toFarsiNumber(n) {
-    //   const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
-    
-    //   return n
-    //     .toString()
-    //     .split('')
-    //     .map(x => farsiDigits[x])
-    //     .join('');
-    // }
-    
-    
-    const handleSelect = (selectedIndex, e) => {
-      setIndex(selectedIndex);
-      setNumber(0)
-    };
-    
-    const handleOrderList = function(index,type){
-      setOrderList( orderList.map((item,i)=>{
-        if(item===orderList[index])return type ==='+' ? {...item,number:item?.number+1} : {...item,number:item?.number-1}
-        return item
-      }))
-    }
-  
-  useEffect(()=>dispatch(orderAction.getProduct()),[dispatch])
+  //   const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
 
-  
-    return (
-        <div className="div--container__product">
-        <Carousel activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
-          {data.map(item=>
-            <Carousel.Item className="carousal--item__prouduct">
-    <img
-      className="d-block w-100"
-      src={item.img}
-      alt="First slide"
-      />
-  </Carousel.Item>
+  //   return n
+  //     .toString()
+  //     .split('')
+  //     .map(x => farsiDigits[x])
+  //     .join('');
+  // }
 
-      )}
-  
-</Carousel>
-    <ToggleButton></ToggleButton>
-    <Detail detail={data[index]}></Detail>
-    <ControlButton index={index} handleOrderList={handleOrderList} data={data[index]} number = {orderList[index]?.number} setNumber = {setNumber} orderList= {orderList} setOrderList = {setOrderList}></ControlButton>
-    <Dialog orderList={orderList[index]} data={orderList.filter(item=>item.number)}></Dialog>
-        </div>
-    )
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+    setNumber(0)
+  };
+
+  const handleOrderList = function (index, type) {
+    setOrderList(orderList.map((item, i) => {
+      if (item === orderList[index]) return type === '+' ? { ...item, number: item?.number + 1 } : { ...item, number: item?.number - 1 }
+      return item
+    }))
+  }
+
+  useEffect(() => dispatch(orderAction.getProduct()), [dispatch])
+  return (
+    <div className="div--container__product">
+      <Carousel activeIndex={index} nextLabel={false} prevLabel={false} onSelect={handleSelect} interval={null} className="carousal--product" >
+        {data.map(item =>
+          <Carousel.Item className="carousal--item__prouduct">
+            <img
+              className="d-block w-100"
+              src={item.img}
+              alt="First slide"
+            />
+          </Carousel.Item>
+
+        )}
+
+      </Carousel>
+      <Row className="d-flex justify-content-center aling-align-items-center col-12 m-0 ">
+        <ToggleButton sizeProduct={setProductSize}></ToggleButton>
+      </Row>
+
+      <Detail detail={data[index]}></Detail>
+      <ControlButton index={index} handleOrderList={handleOrderList} data={data[index]} number={orderList[index]?.number} setNumber={setNumber} orderList={orderList} setOrderList={setOrderList}></ControlButton>
+      <Dialog orderList={orderList[index]} data={orderList.filter(item => item.number)}></Dialog>
+    </div>
+  )
 }
--- src/components/order/toggleButton.js
+++ src/components/order/toggleButton.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
 
 export const ToggleButton = ({ sizeProduct }) => {
 
