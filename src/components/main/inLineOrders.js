d52badb78768e9d47af70e7cc4aea9659f8f8d72
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-07 16:32:41 +0430

    add loader in main

--- src/components/main/inLineOrders.js
+++ src/components/main/inLineOrders.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
-
+import { Container } from 'react-bootstrap';
 //icons
 import { useDispatch, useSelector } from 'react-redux';
 import { orderAction } from '../../actions/orderAction';
 import { InLineOrder } from './inLineOrder';
-
+import { LoaderWhite } from '../base/loader-bg-white'
 
 
 
@@ -12,19 +12,28 @@ import { InLineOrder } from './inLineOrder';
 export const InLineOrders = () => {
 
     const dispatch = useDispatch()
-    const inLineOrders = useSelector(state => state.getInLineOrders.orders)
-    
+    let { loading, orders: inLineOrders } = useSelector(state => state.getInLineOrders)
+
     useEffect(() => {
         dispatch(orderAction.getInLineOrders())
     }, [dispatch])
 
+    console.log(loading);
+
     return (
         <>
             {
-                inLineOrders &&
-                inLineOrders.map((order, index) => 
-                    <InLineOrder order={order} key={index}/>
-                )
+                loading ?
+                    <Container className="d-flex justify-content-center mt-6 ">
+                        <div className="d-flex justify-content-center align-items-center">
+                            <LoaderWhite />
+                        </div>
+                    </Container>
+                    : inLineOrders &&
+                    inLineOrders.map((order, index) =>
+                        <InLineOrder order={order} key={index} />
+                    )
+
             }
         </>
     )
