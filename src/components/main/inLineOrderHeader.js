1d0031d820a519dd0ad6b813d40e23b3ef8ecedc
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-10 18:39:19 +0430

    revers icons in inLineOrderHeader

--- src/components/main/inLineOrderHeader.js
+++ src/components/main/inLineOrderHeader.js
@@ -16,69 +16,69 @@ import chefdoneIcon from './../../assets/images/main/chefdone.svg'
 
 
 
-export const InLineOrderHeader = ({order}) => {
+export const InLineOrderHeader = ({ order }) => {
 
 
     return (
         <>
             <Row className="px-1 " >
-                <Col className="d-flex p-2 mx-2 align-items-center justify-content-start flex-row">
+                <Col className="d-flex p-2 mx-2 align-items-center justify-content-start flex-row-reverse">
                     {
                         order.status.status === 3
-                        ? <><p className="ms-auto main-text-card-queue-condition fw-bold text-center">در حال ارسال</p>
-                            <div className="main-icon-cards main-icon-cards-pass">
-                                <img className="img-fluid" src={deliverydoneIcon} height="35px" alt="deliveryIcon" />
-                            </div></>
-                        : <div className="main-icon-cards ms-2 border border-1 border-dark">
-                            <img className="img-fluid" src={deliveryIcon} height="35px" alt="deliveryIcon" />
-                        </div>
+                            ? <><p className="me-auto main-text-card-queue-condition fw-bold text-center">در حال ارسال</p>
+                                <div className="main-icon-cards main-icon-cards-pass">
+                                    <img className="img-fluid" src={deliverydoneIcon} height="35px" alt="deliveryIcon" style={{ transform: "scale(-1, 1)" }} />
+                                </div></>
+                            : <div className="main-icon-cards me-2 border border-1 border-dark">
+                                <img className="img-fluid" src={deliveryIcon} height="35px" alt="deliveryIcon" style={{ transform: "scale(-1, 1)" }} />
+                            </div>
                     }
                     {
-                        order.status.status === 2 
-                        ? <p className="me-auto ms-1 main-text-card-queue-condition fw-bold">در حال پخت</p>
-                        : null
+                        order.status.status === 2
+                            ? <p className="ms-auto me-1 main-text-card-queue-condition fw-bold">در حال پخت</p>
+                            : null
                     }
                     {
-                        order.status.status === 2 || order.status.status === 3 
-                        ? <div className=" main-icon-cards main-icon-cards-pass ">
-                            <img className="img-fluid" src={coookingdoneIcon} height="35px" alt="coookingIcon" />
-                        </div>
-                        : <div className=" main-icon-cards ms-2 border border-1 border-dark">
-                            <img className="img-fluid" src={coookingIcon} height="35px" alt="coookingIcon" />
-                        </div>
+                        order.status.status === 2 || order.status.status === 3
+                            ? <div className=" main-icon-cards main-icon-cards-pass ">
+                                <img className="img-fluid" src={coookingdoneIcon} height="35px" alt="coookingIcon" />
+                            </div>
+                            : <div className=" main-icon-cards me-2 border border-1 border-dark">
+                                <img className="img-fluid" src={coookingIcon} height="35px" alt="coookingIcon" />
+                            </div>
                     }
                     {
-                        order.status.status === 5 
-                        ?<p className="display-6 me-auto ms-1 main-text-card-queue-condition fw-bold">در حال آماده سازی</p>
-                        : null
+                        order.status.status === 5
+                            ? <p className="display-6 ms-auto me-1 main-text-card-queue-condition fw-bold">در حال آماده سازی</p>
+                            : null
                     }
                     {
-                        order.status.status === 2 || order.status.status === 3 || order.status.status === 5 
-                        ? <div className=" main-icon-cards main-icon-cards-pass ">
-                            <img className="img-fluid" src={chefdoneIcon} height="35px" alt="chefIcon" />
-                        </div>
-                        : <div className=" main-icon-cards ms-2 border border-1 border-dark">
-                            <img className="img-fluid" src={chefIcon} height="35px" alt="chefIcon" />
-                        </div>
+                        order.status.status === 2 || order.status.status === 3 || order.status.status === 5
+                            ? <div className=" main-icon-cards main-icon-cards-pass ">
+                                <img className="img-fluid" src={chefdoneIcon} height="35px" alt="chefIcon" />
+                            </div>
+                            : <div className=" main-icon-cards me-2 border border-1 border-dark">
+                                <img className="img-fluid" src={chefIcon} height="35px" alt="chefIcon" />
+                            </div>
                     }
                     {
                         order.paid && order.status.status === 0
-                        ?<p className="me-auto ms-1 main-text-card-queue-condition fw-bold">در صف انتظار</p>
-                        : null
+                            ? <p className="ms-auto me-1 main-text-card-queue-condition fw-bold">در صف انتظار</p>
+                            : null
                     }
-                    { 
+                    {
                         order.status.status === 2 || order.status.status === 3 || order.status.status === 5 || (order.paid && order.status.status === 0)
-                        ? <div className="main-icon-cards main-icon-cards-pass ms-1">
-                            <img className="img-fluid" src={waitingdoneIcon} alt="deliveryIcon" />
-                        </div>
-                        : <div className="main-icon-cards ms-2 border border-1 border-dark">
-                            <img className="img-fluid" src={waitingIcon} alt="deliveryIcon" />
-                        </div>
+                            ? <div className="main-icon-cards main-icon-cards-pass ">
+                                <img className="img-fluid" src={waitingdoneIcon} alt="deliveryIcon" />
+                            </div>
+                            : <div className="main-icon-cards me-2 border border-1 border-dark">
+                                <img className="img-fluid" src={waitingIcon} alt="deliveryIcon" />
+                            </div>
                     }
                     {
                         (!order.paid) && order.status.status === 0
-                        ? <p className="me-auto  main-text-card-queue-condition fw-bold">در انتظار پرداخت</p>
-                        : null
+                            ? <p className="ms-auto me-1  main-text-card-queue-condition fw-bold">در انتظار پرداخت</p>
+                            : null
                     }
                     <div className="main-icon-cards main-icon-cards-pass me-1">
                         <img className="img-fluid" src={peymentIcon} alt="deliveryIcon" />
