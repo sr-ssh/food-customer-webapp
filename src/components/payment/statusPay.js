9b67907132683f9b631222778a31f83c2b7ea1bd
“saeid” <saeidniyabati@gmail.com>
2021-08-17 17:51:08 +0430

    fixing status pay

--- src/components/payment/statusPay.js
+++ src/components/payment/statusPay.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import logo from './../../assets/images/payment/Payment-online-was-successful.png'
 import logo2 from './../../assets/images/payment/Payment-online-was-unsuccessful.svg'
+import { history } from '../../helpers'
 
 
 export const StausPay = (props) => {
 
     const success= false
+    const handleReturnToApp = function(){
+        history.push({
+                pathname: '/bill'
+            })
+    }
 
     const handlerSuccess = function(success){
             return <div className="success--status__pay">
                  <img src={success?logo:logo2} className="img--success__status-pay"></img>
                  <div className={`div--success__button-status-pay${success?'' : "-faild"}`}>
-                     <span className={`span--success__status-pay`}>بازگشت به هپی پیتزا</span>
+                     <span onClick={handleReturnToApp} className={`span--success__status-pay`}>بازگشت به هپی پیتزا</span>
                  </div>
                  </div>
     }
