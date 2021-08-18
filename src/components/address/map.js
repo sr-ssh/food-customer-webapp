272fdf4e9bb0c17ed4ac374b4d1c52ab685e175b
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-18 19:21:49 +0430

    search address bar done

--- src/components/address/map.js
+++ src/components/address/map.js
@@ -4,14 +4,12 @@ import NeshanMap from 'react-neshan-map-leaflet'
 import markericon from "../../assets/images/address/destination.svg";
 import searchIcon from "../../assets/images/address/search.svg";
 import loctionicon from "../../assets/images/address/loction.svg";
-import deleteicon from "../../assets/images/factor/delete.svg";
 
 
 import { Container, Button, Col, Row, Form, Image, Dropdown } from 'react-bootstrap';
-import { LoaderRed } from '../base/loader-bg-red'
 import "../../assets/styles/leaflet.css"
 import { useDispatch, useSelector } from 'react-redux';
-import { addressActions } from '../../actions/addressActions';
+import { SearchAddressBar } from './searchAddressBar'
 
 
 // YOUR_API_KEY_GOES_BELOW
@@ -20,7 +18,7 @@ const API_KEY = "web.nRDwOvUSAb8WPJZKaJUgdLnXK4MxFukGcw0TieG2";
 export const Map = ({ setAddress, setItem, selectedItem, itemLocation, setItemLocation, setLocating }) => {
 
     const [dimStatus, setDimStatus] = useState(false)
-    const searchedAdrs = useSelector(state => state.searchAddress.searchAddress)
+    const [showAddressBar, setShowAddressbar] = useState(false);
 
     const [mapPropeties, setMapPropeties] = useState()
     const [alerts, setAlert] = useState({});
@@ -71,13 +69,6 @@ export const Map = ({ setAddress, setItem, selectedItem, itemLocation, setItemLo
             }, 3000)
         });
     }
-
-
-    const searchAddress = (e) => {
-        setItem(e.target.value)
-        dispatch(addressActions.searchAddress(e.target.value))
-    }
-
     useEffect(() => {
         setAddress((prevState) => ({ ...prevState, lat: mapPropeties?.myMap.getCenter().lat, lng: mapPropeties?.myMap.getCenter().lng }))
         setAddress((prevState) => ({ ...prevState, lat: itemLocation.lat, lng: itemLocation.lng }))
@@ -89,55 +80,14 @@ export const Map = ({ setAddress, setItem, selectedItem, itemLocation, setItemLo
         <>
             <Col className="px-2 map--search--col ">
                 <Dropdown onToggle={(e) => setDimStatus(!dimStatus)} className="">
-                    {/* <Row className="w-100 justify-content-center inputs"> */}
-                    {/* <Col className="justify-content-center pe-3 ps-0 w-100"> */}
                     <Form.Group controlId="family" className="justify-content-center align-items-center map--search--group">
-                        <Dropdown.Toggle className="d-flex w-100">
+                        <Dropdown.Toggle className="d-flex w-100 px-0" onClick={() => setShowAddressbar(true)}>
                             <Form.Group controlId="family" className="justify-content-center align-items-center map--search--group position-relative">
                                 <Image src={searchIcon} height="10px" alt="loction_icon" className="map--search--icon" />
-                                <Form.Control autocomplete="off" className="h-100 map--search--input" type="text" placeholder="محل مورد نظرتان کجاست؟" onChange={searchAddress} value={selectedItem}
-                                />
+                                <Form.Control autocomplete="off" className="h-100 map--search--input" type="text" placeholder="محل مورد نظرتان کجاست؟" value={selectedItem} />
                             </Form.Group>
                         </Dropdown.Toggle>
-                        {/* <Dropdown.Menu className={`w-75 ${dimStatus ? "dim" : ""} dropdown--address--Menu `} styles={{ transform: "translate3d(42px, 58px, 0px)" }}>
-
-                                    <Form.Group controlId="family" className="justify-content-center align-items-center map--search--group m-2">
-                                        <Image src={searchIcon} height="30px" alt="loction_icon" className="map--search--icon my-3 " />
-                                        <Form.Control autocomplete="off" className="h-100 map--search--input w-100 " type="text" placeholder="محل مورد نظرتان کجاست؟" onChange={searchAddress} value={selectedItem}
-                                        />
-                                        <Dropdown.Toggle className="d-flex">
-                                            <Image src={deleteicon} height="25px" alt="loction_icon" className="close--btn my-2 mx-2" />
-                                        </Dropdown.Toggle>
-                                    </Form.Group>
-
-                                    {searchedAdrs
-                                        ? searchedAdrs.items.map((item, index) => {
-                                            console.log(item);
-                                            return (
-                                                (
-                                                    <Col key={index}>
-                                                        {index ? <Dropdown.Divider /> : null}
-                                                        <Dropdown.Item onClick={() => { setItem(item.title); setItemLocation({ lat: item.location.y, lng: item.location.x }); }}>
-                                                            <Row className="mx-2 d-flex flex-column">
-                                                                <Col className="text-end basket-dropdown-border-left pe-1">
-                                                                    {item.title}
-                                                                </Col>
-                                                                <Col className="text-end basket-dropdown-border-left pe-1">
-                                                                    {item.address}
-                                                                </Col>
-                                                            </Row>
-                                                        </Dropdown.Item>
-                                                    </Col>
-
-                                                ))
-                                        })
-                                        : null
-                                    }
-
-                                </Dropdown.Menu> */}
                     </Form.Group>
-                    {/* </Col> */}
-                    {/* </Row> */}
                 </Dropdown>
             </Col>
 
@@ -177,6 +127,7 @@ export const Map = ({ setAddress, setItem, selectedItem, itemLocation, setItemLo
                 }
                 {alerts?.text}
             </div> */}
+            <SearchAddressBar show={showAddressBar} onHide={() => { setShowAddressbar(false) }} setItem={setItem} selectedItem={selectedItem} setItemLocation={setItemLocation} />
         </>
     )
 }
