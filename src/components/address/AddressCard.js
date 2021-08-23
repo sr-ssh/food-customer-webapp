1f461e261ef9709a1a0f3df70700b290fe4ee485
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-23 16:42:29 +0430

    add delete icon to oldaddress card

--- src/components/address/AddressCard.js
+++ src/components/address/AddressCard.js
@@ -3,10 +3,12 @@ import { Row, Col, Card } from 'react-bootstrap';
 
 // Assets
 import editIcon from '../../assets/images/main/edit.svg'
+import deleteIcon from '../../assets/images/address/delet.svg'
+
 import { history } from '../../helpers';
 
 
-export const AddressCard = ({address, setAddress, selected}) => {
+export const AddressCard = ({ address, setAddress, selected }) => {
 
     const editAddress = (address) => {
         history.push({
@@ -17,22 +19,27 @@ export const AddressCard = ({address, setAddress, selected}) => {
 
 
     return (
-        
+
         <Col onClick={() => setAddress(address)}>
-            <Card className={`m-auto mx-2 mt-4 bg-white old--address-card lh-lg pb-2  ${selected ? "address--red--border" : null}`} >
-                <Card.Body className="p-0 pe-2 py-2 text-gray">
-                    <Row className="pe-2">
-                        <Row className="mt-2 d-flex flex-nowrap align-items-center">
-                            <Col className="col-10">
+            <Card className={`m-0 mx-2 mt-4 bg-white old--address-card lh-lg  ${selected ? "address--red--border" : null}`} >
+                <Card.Body className="px-2 py-2 text-gray">
+                    <Row className="px-2 d-flex flex-column  ">
+                        <Row className="mt-1 d-flex flex-nowrap flex-row">
+                            <Col className="col-12">
                                 <Card.Text>
                                     {address.address}
                                 </Card.Text>
                             </Col>
                         </Row>
-                        <Row>
+                        <Row className="justify-content-end ms-0 ps-0">
                             <Col className="ps-0 me-1 col-auto d-flex justify-content-end" onClick={() => editAddress(address)}>
                                 <Card.Link>
                                     <img src={editIcon} height="35px" alt="edit-icon" />
+                                </Card.Link>
+                            </Col>
+                            <Col className="ps-0 me-1 col-auto d-flex justify-content-end">
+                                <Card.Link>
+                                    <img src={deleteIcon} height="35px" alt="edit-icon" />
                                 </Card.Link>
                             </Col>
                         </Row>
--- src/components/address/oldAddress.js
+++ src/components/address/oldAddress.js
@@ -10,16 +10,16 @@ import addIcon from '../../assets/images/main/add-red.svg'
 import { AddressCard } from './AddressCard';
 
 
-export const OldAddress = ({addresses, setNewAddress}) => {
+export const OldAddress = ({ addresses, setNewAddress }) => {
 
     const [address, setAddress] = useState({})
     const addressData = useSelector(state => state.newAddress);
     const dispatch = useDispatch()
 
     const selectAddress = () => {
-        if(address.address)
-            dispatch(addressActions.newAddress({lng: address.GPS.coordinates[0], lat: address.GPS.coordinates[1], address: address.address}))
-        else{
+        if (address.address)
+            dispatch(addressActions.newAddress({ lng: address.GPS.coordinates[0], lat: address.GPS.coordinates[1], address: address.address }))
+        else {
             dispatch(alertActions.error('یک ادرس را انتخاب کنید'))
             setTimeout(() => {
                 dispatch(alertActions.clear());
@@ -33,11 +33,11 @@ export const OldAddress = ({addresses, setNewAddress}) => {
                 <div id="old--address-scroller-container-cards">
                     <Container className="m-0">
                         <Row className="d-flex flex-column" >
-                            
+
                             {
-                                addresses.map((addresss, index) => <AddressCard key={index} address={addresss} setAddress={setAddress} selected={address === addresss ? true : false}/>)
+                                addresses.map((addresss, index) => <AddressCard key={index} address={addresss} setAddress={setAddress} selected={address === addresss ? true : false} />)
                             }
-                                
+
                         </Row>
                     </Container>
                 </div>
@@ -45,14 +45,14 @@ export const OldAddress = ({addresses, setNewAddress}) => {
             <Container fluid className="m-0 d-flex flex-column ms-0 align-items-center justify-content-end main-add-address ">
                 <Row className="m-0 mb-2 ">
                     <Col className="col-12 px-0">
-                        <Button className=" d-flex flex-row justify-content-center align-items-center btn--checkout--order btn--red--two" onClick={() => setNewAddress(true)}>
-                            <img className="mx-2" src={addIcon} height="25px" alt="edit-icon" /><span className="mx-3 ">آدرس جدید</span>
+                        <Button className=" d-flex flex-row justify-content-center align-items-center btn--checkout--order btn--dark--one" onClick={() => setNewAddress(true)}>
+                            <img className="mx-2" src={addIcon} height="25px" alt="edit-icon" /><span className="mx-3 text-light ">آدرس جدید</span>
                         </Button>
                     </Col>
                 </Row>
                 <Row className="m-0 mb-2 w-100  ">
                     <Col className="col-12 px-0">
-                    <Button type="submit" className="col-12 py-3 d-flex flex-row justify-content-center align-items-center btn--add--new--address btn--red--one" onClick={selectAddress}>
+                        <Button type="submit" className="col-12 py-3 d-flex flex-row justify-content-center align-items-center btn--add--new--address btn--red--one" onClick={selectAddress}>
                             {
                                 addressData.loading ?
                                     <LoaderRed />
