fc55e99e5cfabf93058d0066e41cec4d449e0d6a
mohammadjvad9 <mohmadjavadheidari9@gmail.com>
2021-08-27 20:20:26 +0430

    new edit map address

--- src/components/address/editAddress.js
+++ src/components/address/editAddress.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Container, Row, Col, Card, Form, Button, Alert } from 'react-bootstrap';
 import { LoaderRed } from '../base/loader-bg-red'
-import { Map } from "./map";
 import { addressActions } from '../../actions/addressActions';
 
 // Components
 import { Header } from '../base/addressHeader'
+import { StaticMap } from './staticMap'
+import { Maps } from "./maps";
 
 export const EditAddress = (props) => {
 
-    const [oldLoc, setOldLoc] = useState({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0], address: props?.location.state?.address.address });
-    const [address, setAddress] = useState({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0], address: props?.location.state?.address.address });
+    const [oldLoc, setOldLoc] = useState({});
+    const [address, setAddress] = useState({});
     const [validate, setValidate] = useState(false);
     const [selectedItem, setItem] = useState("")
-    const [itemLocation, setItemLocation] = useState({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0] })
-    const [locating, setLocating] = useState(false)
+    const [itemLocation, setItemLocation] = useState({})
+    const [isPickAddress, setIsPickAddress] = useState(false)
     let alertMessage = useSelector(state => state.alert.message);
     let alerType = useSelector(state => state.alert.type);
 
@@ -36,10 +37,20 @@ export const EditAddress = (props) => {
             setValidate(true)
     }
 
+
+    useEffect(() => {
+        if (props.location.state != undefined) {
+            setOldLoc({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0], address: props?.location.state?.address.address });
+            setAddress({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0], address: props?.location.state?.address.address });
+            setItemLocation({ lat: props?.location.state?.address.GPS.coordinates[1], lng: props?.location.state?.address.GPS.coordinates[0] })
+        }
+    }, [props.location.state])
+
+
     return (
         <>
             <div className="address--page">
-                <Header title="تغییر آدرس" backLink="/address" backtext="آدرس ها" locating={locating} />
+                <Header title="تغییر آدرس" backLink="/address" backtext="آدرس ها" locating={false} />
                 {
                     alertMessage &&
                     <>
@@ -51,17 +62,22 @@ export const EditAddress = (props) => {
                     </>
                 }
                 <Container className="m-0 mx-auto new--address--container d-flex flex-column justify-content-between">
-                    <Row style={{ height: "61vh", position: "relative" }}>
-                        <Col style={{ height: "61vh", position: "relative" }} className="p-0">
-                            <Map className="map" setAddress={setAddress} selectedItem={selectedItem} setItem={setItem} itemLocation={itemLocation} setItemLocation={setItemLocation} setLocating={setLocating} />
-                        </Col>
-                    </Row>
+                    <div className="m-0 px-3 mt-3 w-100">
+                        <Row className="mb-2">
+                            <span className="label--maps">لطفا موقعیت خود را انتخاب کنید</span>
+                        </Row>
+                        <Row className="row--static--map" onClick={() => setIsPickAddress(true)}>
+                            <Col className="p-0 w-100">
+                                <StaticMap address={address} />
+                            </Col>
+                        </Row>
+                    </div>
                     <Row className="m-0 mt-2 mb-auto new--address--inputs">
                         <Col className="p-0 new--address--description-input">
                             <Form.Group controlId="description">
                                 <Form.Label className="pe-2">لطفا آدرس دقیق را بنویسید</Form.Label>
                                 <Card className="border-0 bg-transparent" >
-                                    <Form.Control as="textarea" name="description" value={address.address} onChange={addressHandler} className="new--address--exact--text-container" isValid={address.description && validate && true} isInvalid={!address.description && validate && true} required />
+                                    <Form.Control as="textarea" name="description" onChange={addressHandler} className="new--address--exact--text-container" isValid={address.description && validate && true} isInvalid={!address.description && validate && true} required />
                                 </Card>
                             </Form.Group>
                         </Col>
@@ -72,12 +88,12 @@ export const EditAddress = (props) => {
                                 {
                                     addressData.loading ?
                                         <LoaderRed />
-                                        : <span className="ps-2">ثبت</span>
+                                        : <span className="ps-2">ادامه</span>
                                 }
                             </Button>
                         </Col>
                     </Row>
-
+                    <Maps show={isPickAddress} onHide={() => { setIsPickAddress(false) }} setAddress={setAddress} selectedItem={selectedItem} setItem={setItem} itemLocation={itemLocation} setItemLocation={setItemLocation} />
                 </Container>
             </div>
         </>
